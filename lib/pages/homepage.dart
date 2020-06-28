1b68442242408b8f2e46f15989094feddb0cf1cf
Govamsh <37132611+Govamsh@users.noreply.github.com>
2020-06-28 12:40:36 +0530

    Demo Home page added to the existing application

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,14 +1,12 @@
 import 'package:boringproject/bloc.navigation_bloc/navigation_bloc.dart';
+import 'package:boringproject/screens/note_list.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget with NavigationStates{
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "HomePage",
-        style: TextStyle( fontWeight: FontWeight.w900, fontSize: 28),
-      ),
+    return Container(
+      child: NoteList(),
     );
   }
 
